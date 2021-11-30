a269ddb22f4e239b63af4e1db602d37ebafbce38
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-11-30 11:00:09 +0200

    added axios get calls for weather

--- src/components/CountrySingle.js
+++ src/components/CountrySingle.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
+import axios from "axios";
+function getCountry(capital) {
+  return axios.get(`https://restcountries.com/v2/capital/${capital}`);
+}
+
+function getWeather(capital) {
+  return axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+  );
+}
 
 class CountrySingle extends Component {
+  state = {
+    country: [],
+    weather: [],
+  };
+  componentDidMount() {
+    // we can use console log in this way in a class component
+    console.log("wow");
+    console.log(this.props.params.name);
+  }
   render() {
     return (
       <div className="single">
