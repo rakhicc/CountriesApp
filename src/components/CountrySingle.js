e9aeccd8a93ee53a6d86fc8eb27fdc09857d9c60
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-11-30 12:14:17 +0200

    the response from promises being set to weather and country using setstate

--- src/components/CountrySingle.js
+++ src/components/CountrySingle.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import axios from "axios";
-function getCountry(capital) {
+const getCountry = (capital) => {
   return axios.get(`https://restcountries.com/v2/capital/${capital}`);
-}
+};
 
-function getWeather(capital) {
+const getWeather = (capital) => {
   return axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
   );
-}
+};
 
 class CountrySingle extends Component {
   state = {
-    country: [],
-    weather: [],
+    country: {},
+    weather: {},
   };
   componentDidMount() {
     // we can use console log in this way in a class component
     console.log("wow");
     console.log(this.props.params.name);
+    Promise.all([
+      getCountry(this.props.params.name),
+      getWeather(this.props.params.name),
+    ]).then((res) => {
+      console.log(res);
+      this.setState({ country: res[0].data[0], weather: res[1].data });
+      console.log("country", this.state.country);
+      console.log("weather", this.state.weather);
+    });
   }
   render() {
     return (
